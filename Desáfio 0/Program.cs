b644fdd71f16a7b5dedf47a7b1a287d41de0516f
PandaMusics <87098275+PandaMusics@users.noreply.github.com>
2023-01-04 16:10:50 -0300

    Add files via upload

---
+++ Desáfio 0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Desafio0
+{
+    // As variáveis podem ser escritas aqui também
+
+    private static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 3;
+
+        Console.WriteLine(a + b);
+
+        Console.ReadKey();
+    }
+}
---
+++ Desáfio 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Desafio1
+{
+    string nome = "Fortnite";
+
+    private static void Main(string[] args)
+    {
+        Desafio1 program = new Desafio1();
+
+        Console.WriteLine(program.nome + " < " + MudarString(program.nome, "Minecraft"));
+
+        Console.ReadKey();
+    }
+
+    private static string MudarString(string variavel, string nome)
+    {
+        variavel = nome;
+
+        return variavel;
+    }
+}
