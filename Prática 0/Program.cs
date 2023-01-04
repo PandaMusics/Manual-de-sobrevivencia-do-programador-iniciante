8e5a1c0766d02f5d4eee44df1d4389fffb2e4c97
PandaMusics <87098275+PandaMusics@users.noreply.github.com>
2023-01-04 11:39:03 -0300

    Add files via upload

---
+++ Prática 0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // Referência a biblioteca padrão
+
+class Prática0 // Classe do programa
+{
+    private static void Main(string[] args) // Será explicado depois no livro!
+    {
+        Console.WriteLine("Escreva o que quiser aqui!"); // Comando da biblioteca (System) que escreve algo no console
+                                                         // É usado para testar códigos e achar errors ou bugs
+
+        Console.ReadKey(); // Comando da biblioteca (System) que espera o usuário apertar qualquer tecla do seu teclado
+    }
+}
